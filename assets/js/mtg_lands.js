1b6365a0cddf9c2bc49cb0b4af807382e764e6e4
Tom Clancy is Dead <tomclancyisdeadagain@gmail.com>
2018-08-17 10:13:16 -0700

    Initial commit. Using Solid State html5 template and has working filtering by land type and color

---
+++ assets/js/mtg_lands.js
@@ -0,0 +1,92 @@
+(function($) {
+    cards=cards_data;
+
+    function card_colors(card){
+        return [...card.color].concat("");
+    }
+    function color_match(card, land_type, checked_colors){
+        if (land_type === "dual land")
+            required_color_matches = checked_colors.length >= 2 ? 2 : 1;
+        else
+            required_color_matches = 1;
+        color_matches = card_colors(card).filter(n => checked_colors.includes(n));
+        return (color_matches.length >= required_color_matches);
+    }
+
+    function get_cycles(data, land_type, color){
+        var colors = color ? [...color] : [""];
+        cycles = _.chain(data)
+            .filter({type: `${land_type}`})
+            .filter(function(card) {return color_match(card, land_type, colors);})
+            .groupBy("set")
+            .groupBy("cycle")
+            .value();
+
+        return(_.valuesIn(cycles));
+    }
+    function clear_cards(){
+        $("#wrapper").empty()
+    }
+    function show_cycles(cycles) {
+        clear_cards();
+        // loop through cycles, creating different section for each
+        _(cycles).forEach(function(cycle, i){
+            $cycle = $("<div>", {class: `wrapper spotlight cycle style${(i%3)+1}` })
+            if(i%2===1){
+                $cycle.addClass("alt");
+            }
+            // console.log($div)
+            $("#wrapper").append($cycle);
+            // loop through sets, creating different subsections and displaying cards
+            _(cycle).forEach(function(set){
+                $set =$("<div>", {class: "set", id: `${set[0].cycle}-${set[0].set}`})
+                $cycle.append($set)
+                _(set).forEach(function(card){
+                    $card = $(`<span>${card.name}<img src="${card.image_url}"></span>`);
+                    $set.append($card);
+                });
+            });
+        });
+    }
+    // function five_colors(data){
+    //     five_color_lands = _.chain(data)
+    //         .filter({type: "five color"})
+    //         .sortBy("set")
+    //         .value();
+    //     return(_.valuesIn(five_color_lands));
+    // }
+    // function show_cards(cards){
+    //     clear_cards();
+    //     $div = $("<div>", {class: `wrapper spotlight style1` });
+    //     $("#wrapper").append ($div);
+    //     _(cards).forEach(function(card){
+    //         $card = $(`<span>${card.name}</span>`);
+    //         $div.append($card);
+    //     });
+    // }
+    function land_colors(){
+        color = ""
+        $("input[type=checkbox][name=land-colors]:checked").each(function(){color += $(this).val()})
+        return color;
+    }
+
+    $("input[type=radio][name=land-type]").change(function(){
+        switch (this.value){
+            case "dual land":
+                $("#land-colors").show();
+                break;
+            case "five color":
+                $("#land-colors").hide();
+                break;
+            case "spell land":
+                $("#land-colors").show();
+            case "utility land":
+                $("#land-colors").show();
+        }
+        show_cycles(get_cycles(cards, $("input[type=radio][name=land-type]:checked").val(), land_colors()));
+    });
+    $("input[type=checkbox][name=land-colors]").change(function(){
+        show_cycles(get_cycles(cards, $("input[type=radio][name=land-type]:checked").val(), land_colors()));
+    })
+    show_cycles(get_cycles(cards, "dual land"))
+})(jQuery);
---
+++ data/sample.js
@@ -0,0 +1,58 @@
+cards_data = [
+  {
+    "name": "Dragonskull Summit",
+    "set": "XLN",
+    "type": "dual land",
+    "cycle": "Checklands",
+    "color": "BR",
+    "image_url": "images\\cards\\xln-252-dragonskull-summit.jpg"
+  },
+  {
+    "name": "Rootbound Crag",
+    "set": "XLN",
+    "type": "dual land",
+    "cycle": "Checklands",
+    "color": "RG",
+    "image_url": "images\\cards\\xln-256-rootbound-crag.jpg"
+  },
+  {
+    "name": "Unclaimed Territory",
+    "set": "XLN",
+    "type": "five color",
+    "cycle": "",
+    "color": "WUBRG",
+    "image_url": "images\\cards\\xln-258-unclaimed-territory.jpg"
+  },
+  {
+    "name": "Clifftop Retreat",
+    "set": "DOM",
+    "type": "dual land",
+    "cycle": "Checklands",
+    "color": "RW",
+    "image_url": "images\\cards\\dom-239-clifftop-retreat.jpg"
+  },
+  {
+    "name": "Hashep Oasis",
+    "set": "HOU",
+    "type": "spell land",
+    "cycle": "Spell Desert",
+    "color": "G",
+    "image_url": "images\\cards\\hou-177-hashep-oasis.jpg"
+  },
+  {
+    "name": "Arch of Orazca",
+    "set": "RIX",
+    "type": "spell land",
+    "cycle": "",
+    "color": "C",
+    "image_url": "images\\cards\\rix-185-arch-of-orazca.jpg"
+  },
+  {
+    "name": "Zhalfirin Void",
+    "set": "DOM",
+    "type": "utility land",
+    "cycle": "",
+    "color": "C",
+    "image_url": "images\\cards\\dom-249-zhalfirin-void.jpg"
+  }
+]
